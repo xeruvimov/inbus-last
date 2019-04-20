5de37140200da71df0bc04052ef0f110e30f2d91
Xeruvimov <xeruvimov@gmail.com>
2019-04-20 14:38:03 +0400

    back in progress

---
+++ src/main/java/rest/OrderRestController.java
@@ -0,0 +1,35 @@
+package rest;
+
+import model.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import serviceinterface.OrderService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/orders")
+public class OrderRestController {
+    @Autowired
+    private OrderService orderService;
+
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<Order>> getAllPlayers() {
+        List<Order> result = this.orderService.getAll();
+        if (result.isEmpty()) {
+            return new ResponseEntity<List<Order>>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<List<Order>>(result, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Order> addPlayer(@RequestBody Order order) {
+        return new ResponseEntity<Order>(orderService.save(order), HttpStatus.OK);
+    }
+}
