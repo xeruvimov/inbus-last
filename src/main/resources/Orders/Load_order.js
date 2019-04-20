e42c6900d6bc19c0167b4c1ce8affe4687f21fe0
Дмитрий <@DESKTOP-N4D3ITB>
2019-04-20 14:44:35 +0400

    web page

---
+++ src/main/resources/Orders/Load_order.js
@@ -0,0 +1,32 @@
+var isOpen = [false, false, false, false];
+
+function addDescription(id) {
+    if (isOpen[id]) {
+        let order = document.getElementById(id);
+        let div = order.getElementsByClassName("description").item(0);
+        console.log(div);
+        order.removeChild(div);
+        isOpen[id] = false;
+    } else {
+        let order = document.getElementById(id);
+        let description = createDescription();
+        order.appendChild(description);
+        isOpen[id] = true;
+    }
+}
+
+function createDescription() {
+    let div = document.createElement("div");
+    div.setAttribute("class", "description");
+    let h4 = document.createElement("h4");
+    h4.setAttribute("align", "center");
+    h4.textContent = "Описание";
+    div.appendChild(h4);
+    p = document.createElement("p");
+    p.textContent = "test";
+    div.appendChild(p);
+    let img = document.createElement("img");
+    img.setAttribute("src", "Img/gps_label.png");
+    img.setAttribute("width", "")
+    return div;
+}
