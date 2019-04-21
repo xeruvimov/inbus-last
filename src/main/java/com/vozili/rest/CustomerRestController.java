9b6bf58645c1e0ff84656b66b96bdb42c8071812
Xeruvimov <xeruvimov@gmail.com>
2019-04-21 15:26:15 +0400

    add booked service

--- src/main/java/com/vozili/rest/CustomerRestController.java
+++ src/main/java/com/vozili/rest/CustomerRestController.java
@@ -2,12 +2,15 @@ package com.vozili.rest;
 
 import com.vozili.model.Customer;
 import com.vozili.model.Order;
+import com.vozili.repository.CustomerRepository;
 import com.vozili.serviceinterface.CustomerService;
+import com.vozili.serviceinterface.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,6 +19,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class CustomerRestController {
     @Autowired
     private CustomerService customerService;
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @RequestMapping(value = "/booked", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<Order> getAllBookedOrder() {
@@ -42,5 +51,28 @@ public class CustomerRestController {
             return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<Customer>(customer, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/booked/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Customer> getAllBookedOrder(@PathVariable Long id) {
+        Customer result = customerService.getCustomer();
+        Order order = orderService.getById(id);
+        result.setBookedOrder(order);
+        customerRepository.save(result);
+
+        if (order == null) {
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Customer>(result, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/booked", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Customer> deleteBookedOrder() {
+        Customer result = customerService.getCustomer();
+        result.setBookedOrder(null);
+        customerRepository.save(result);
+
+        return new ResponseEntity<Customer>(result, HttpStatus.OK);
     }
 }
